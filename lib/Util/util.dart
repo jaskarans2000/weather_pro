b762151a2372614252da745d8cb25dadc07af0fa
Jaskaran Singh <36985639+jaskarans2000@users.noreply.github.com>
2020-04-16 14:51:05 +0530

    Update util.dart

--- lib/Util/util.dart
+++ lib/Util/util.dart
@@ -1,7 +1,7 @@
 import 'package:intl/intl.dart';
 
 class Util{
-  static String appID="98f220ca3dc1181dc396e31d1d74573a";
+  static String appID="appId";
 
   static String getFormattedDate(DateTime dateTime){
     return DateFormat("EEEE,d,MMMM,y").format(dateTime);
